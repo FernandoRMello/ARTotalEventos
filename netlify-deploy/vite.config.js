d094b849ac18332391c91ff532bd28a0f586c2b7
FernandoRMello <161377013+FernandoRMello@users.noreply.github.com>
2025-06-28 01:06:23 -0300

    Update vite.config.js

--- netlify-deploy/vite.config.js
+++ netlify-deploy/vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': resolve(__dirname, 'src'),
     },
   },
-})
+  build: {
+    outDir: 'dist',
+  },
+});
