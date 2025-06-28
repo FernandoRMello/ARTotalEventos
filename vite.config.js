6fa266c6890b80cd736c3cb7bd1b7f4d7b169dd3
FernandoRMello <161377013+FernandoRMello@users.noreply.github.com>
2025-06-28 01:16:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
-});
+plugins: [react()],
+  base: './', // MUITO IMPORTANTE para rodar em Netlify no root
+})
+ 
